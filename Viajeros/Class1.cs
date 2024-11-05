c6154e1ddcd7d2306af4a94eb28f4bfe7e1c544c
Andrés <4145808@alu365.murciaeduca.es>
2024-11-05 13:00:46 +0100

    Agregar archivos de proyecto.

---
+++ Viajeros/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viajeros
+{
+    public class Class1
+    {
+    }
+}
